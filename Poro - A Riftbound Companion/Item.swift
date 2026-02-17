6bbcbf51c5fabdfefc981bec1df19545bbc0704d
Bruno <brunodanielsf14@gmail.com>
2026-02-17 14:51:43 +0000

    Initial Commit

---
+++ Poro - A Riftbound Companion/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Poro - A Riftbound Companion
+//
+//  Created by Bruno Ferreira on 17/02/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
